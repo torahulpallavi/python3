b3027cb82f6f74af14808589f2299d71485b4b1d
rahul <rp@gmail.com>
2019-06-08 21:08:09 +1000

    add

--- scripts/testdat.py
+++ scripts/testdat.py
@@ -1,8 +1,18 @@
 import psycopg2
 import csv
-conn = psycopg2.connect("host=localhost dbname=postgres user=postgres password=pa55word")
+conn = psycopg2.connect("host=localhost dbname=postgres user=postgres password=postgres")
 print "connection successfull"
 cur = conn.cursor()
+
+with open('name.csv', 'r') as f:
+    cur.copy_from(f, 'name', sep=',') # name of the table and seprator
+    conn.commit()
+
+    print "[INFO] data loaded successfully" 
+
+
+'''
+
 with open('name.csv', 'r') as f:
     reader = csv.reader(f)
     next(reader)  # Skip the header row.
@@ -11,7 +21,12 @@ with open('name.csv', 'r') as f:
             "INSERT INTO name VALUES (%s, %s)",
             row
         )
-conn.commit()
+'''
+
+
+
+
+
 
 
 
